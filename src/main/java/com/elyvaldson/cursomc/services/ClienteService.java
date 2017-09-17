d05193818c5e4f88bde144c75de0e4152845196e
Elyvaldson <elyvaldson@hotmail.com>
2017-09-17 13:41:28 -0300

    Endpoint /clientes/{id}

---
+++ src/main/java/com/elyvaldson/cursomc/services/ClienteService.java
@@ -0,0 +1,23 @@
+package com.elyvaldson.cursomc.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.elyvaldson.cursomc.domain.Cliente;
+import com.elyvaldson.cursomc.repositories.ClienteRepository;
+import com.elyvaldson.cursomc.services.exceptions.ObjectNotFoundException;
+
+@Service
+public class ClienteService {
+	
+	@Autowired
+	private ClienteRepository repo;
+	
+	public Cliente buscar(Integer id) {
+		Cliente obj = repo.findOne(id);
+		if (obj == null) {
+			throw new ObjectNotFoundException("Objeto não encontrado! Id: " + id + ", Tipo: " + Cliente.class.getName());
+		}
+		return obj;
+	}
+}
