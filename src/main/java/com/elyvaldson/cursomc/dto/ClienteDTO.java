e56627da53bfa7d18a1f5480559027f6f17fbf27
Elyvaldson <elyvaldson@hotmail.com>
2017-10-31 22:03:01 -0200

    Validacao custumizada: email não repetido na atualizacao de Cliente

--- src/main/java/com/elyvaldson/cursomc/dto/ClienteDTO.java
+++ src/main/java/com/elyvaldson/cursomc/dto/ClienteDTO.java
@@ -7,7 +7,9 @@ import org.hibernate.validator.constraints.Length;
 import org.hibernate.validator.constraints.NotEmpty;
 
 import com.elyvaldson.cursomc.domain.Cliente;
+import com.elyvaldson.cursomc.services.validations.ClienteUpdate;
 
+@ClienteUpdate
 public class ClienteDTO implements Serializable {
 	private static final long serialVersionUID = 1L;
 	
